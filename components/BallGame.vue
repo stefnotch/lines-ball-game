7c716314978041255987fef25ec98e69c659ae25
Stefnotch <brandmairstefan@gmail.com>
2020-05-13 10:06:25 +0200

    Add canvas with drawing function

--- components/BallGame.vue
+++ components/BallGame.vue
@@ -1,39 +1,60 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <button @click="count++">count is: {{ count }}</button>
-    <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
-  </div>
+  <canvas class="game-canvas" ref="canvas"></canvas>
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  },
-  data() {
-    return {
-      count: 0
-    }
-  }
+<script lang="ts">
+import {
+  defineComponent,
+  ref,
+  watchEffect,
+  compile,
+  computed,
+  onUnmounted,
+  onBeforeUnmount
+} from "vue";
+
+function useCanvas() {
+  const canvas = ref<HTMLCanvasElement>();
+  const ctx = computed(() =>
+    canvas.value ? canvas.value.getContext("2d") : undefined
+  );
+
+  onBeforeUnmount(() => {
+    canvas.value = undefined;
+  });
+
+  return {
+    canvas,
+    ctx
+  };
 }
+
+export default defineComponent({
+  setup() {
+    const { canvas, ctx } = useCanvas();
+
+    function draw(timestamp: number) {
+      if (!canvas.value) return;
+      if (!ctx.value) return;
+
+      canvas.value.width = window.innerWidth;
+      canvas.value.height = window.innerHeight;
+
+      requestAnimationFrame(draw);
+    }
+
+    watchEffect(() => {
+      if (ctx.value) {
+        requestAnimationFrame(draw);
+      }
+    });
+
+    return {
+      canvas
+    };
+  }
+});
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
