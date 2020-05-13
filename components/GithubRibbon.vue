d6a6156adae16d5453754271a527bc247ea448fa
Stefnotch <brandmairstefan@gmail.com>
2020-05-13 09:22:55 +0200

    Fix GitHub ribbon

--- components/GithubRibbon.vue
+++ components/GithubRibbon.vue
@@ -1,17 +1,20 @@
 <template>
   <!-- From https://github.com/tholman/github-corners -->
-  <a
-    href="https://github.com/stefnotch/lines-ball-game"
-    class="github-corner"
-    aria-label="View source on GitHub"
-  >
+  <div class="github-corner">
     <svg
-      width="80"
-      height="80"
+      width="70"
+      height="70"
       viewBox="0 0 250 250"
       style="fill:#151513; color:#fff; position: absolute; top: 0; border: 0; right: 0;"
       aria-hidden="true"
     >
+      <a
+        href="https://github.com/stefnotch/lines-ball-game"
+        aria-label="View source on GitHub"
+        style="pointer-events: auto"
+      >
+        <path d="M0,0 L250,250 L250,0 Z" />
+      </a>
       <path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z" />
       <path
         d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2"
@@ -25,11 +28,14 @@
         class="octo-body"
       />
     </svg>
-  </a>
+  </div>
 </template>
 <style scoped>
 .github-corner:hover .octo-arm {
   animation: octocat-wave 560ms ease-in-out;
+}
+.github-corner {
+  pointer-events: none;
 }
 @keyframes octocat-wave {
   0%,
